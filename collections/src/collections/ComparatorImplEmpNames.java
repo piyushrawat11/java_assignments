0919c83f1c4a3edf8c51064f7715f5d253cf94ff
piyushrawat11 <73756055+piyushrawat11@users.noreply.github.com>
2022-11-02 23:48:34 +0530

    Add files via upload

---
+++ collections/src/collections/ComparatorImplEmpNames.java
@@ -0,0 +1,13 @@
+package collections;
+
+import java.util.Comparator;
+
+public class ComparatorImplEmpNames implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

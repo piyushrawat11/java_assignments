347562616bac214a9186b2c5f51dea0889094045
piyushrawat11 <73756055+piyushrawat11@users.noreply.github.com>
2022-11-04 11:41:48 +0530

    Create StringBuilderEx.java

---
+++ StringBuilderEx.java
@@ -0,0 +1,62 @@
+package learnJava;
+
+public class StringBuilderEx {
+
+	public static void main(String[] args) {
+
+		// immutable concept of String
+		String s = "Scarllet";
+
+		System.out.println(s + " Johanasson");
+
+		s = s + " Johanasson";
+		String s1 = s + " Johanasson";
+		System.out.println(s);
+		System.out.println(s1);
+
+		String s2 = " Johanasson";
+
+		// String imp conversions
+
+		// char to String
+		char ch = 'c';
+
+		String chstr = Character.toString(ch);
+		System.out.println("char ch=" + ch + " to String:" + chstr);
+
+		// String to char
+
+		String yesno = "c";
+
+		ch = yesno.charAt(0);
+		System.out.println("String " + yesno + " to char:" + ch);
+		System.out.println(ch);
+
+		// char to unicode value
+
+		ch = 'A';
+		Character x = ch; // auto-boxing char to Character
+
+		int charval = Character.getNumericValue(ch);
+
+		System.out.println("char ch=" + ch + " to Unicode value:" + charval);
+
+		// important methods of string
+
+		String str = "Let's learn something";
+
+		String str1 = str.substring(6, 8);
+		System.out.println(str1);
+
+		str1 = str.substring(5);
+		System.out.println(str1);
+
+		System.out.println(str.toLowerCase());
+
+		// String to char array
+		char[] arr = str.toCharArray();
+		System.out.println(arr);
+
+	}
+
+}

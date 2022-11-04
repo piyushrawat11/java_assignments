e50f6e5541ee4ea61cc839b7b5f1181b28a7688e
piyushrawat11 <73756055+piyushrawat11@users.noreply.github.com>
2022-11-04 11:40:35 +0530

    Update HashAccountmap.java

--- HashAccountmap.java
+++ HashAccountmap.java
@@ -2,7 +2,7 @@ package collections;
 
 import java.util.*;
 
-public class HashAccountMap {
+public class HashAccountmap {
 
 	public static void main(String[] args) {
 		Map<account, Double> map = new HashMap<>();
